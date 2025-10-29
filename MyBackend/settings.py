e918e70983c2044491f8cb71601b72a4133a7f2a
Jmgarcia30 <jmjgarcia.student@ua.edu.ph>
2025-10-29 08:31:42 +0800

    Allowed Host

--- MyBackend/settings.py
+++ MyBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7o+j9zoap&08evx@g43g4)!fcw3h!hb=jxoi9f=&wq5u-&25&+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'http://192.168.30.109:8000/', '192.168.30.109:8000', '192.168.30.109']
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'http://192.168.30.109:8000/', '192.168.30.109:8000', '192.168.30.109', 'peitel-backend-iyus.onrender.com']
 
 # Application definition
 
