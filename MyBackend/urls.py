0702b7e1cdb6f27fa186d3b4fa9e2454acc13e85
Jmgarcia30 <jmjgarcia.student@ua.edu.ph>
2025-11-25 09:21:33 +0800

    Added add users, edit and delete on backend

--- MyBackend/urls.py
+++ MyBackend/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = 'registration'
 
 
@@ -27,3 +30,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/', include('registration.urls')), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
